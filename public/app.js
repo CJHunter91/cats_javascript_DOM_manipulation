12c5c52d10ae9779febf7a51ff56e42b6c487e9d
Chris Hunter <orkem2@gmail.com>
2017-09-04 15:48:11 +0100

    added app function to create new cat listing

--- public/app.js
+++ public/app.js
@@ -1,6 +1,31 @@
 
 var app = function (){
 
+  var newListItem = document.createElement('ul');
+  newListItem.classList.add('cat')
+
+  var name = document.createElement('li');
+  var food = document.createElement('li');
+  var imgContainer = document.createElement('li');
+  var img = document.createElement('img');
+
+  img.src = "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg"
+  img.width = 500;
+  imgContainer.appendChild(img);
+
+  var section = document.getElementById('cats');
+
+
+  name.innerText = "Mr BigglesWorth";
+  food.innerText = "Kibble N'Bits"
+
+
+  newListItem.appendChild(name);
+  newListItem.appendChild(food);
+  newListItem.appendChild(imgContainer);
+  section.appendChild(newListItem);
+
+
 }
 
 window.onload = app;
