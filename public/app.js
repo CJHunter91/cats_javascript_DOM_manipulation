a6bf74e951afa6ed7051af2832d34e1fc522a861
Chris Hunter <orkem2@gmail.com>
2017-09-04 16:02:12 +0100

    created createNewCat function

--- public/app.js
+++ public/app.js
@@ -1,30 +1,38 @@
 
 var app = function (){
+  
+  var createNewCat = function(name, food, image){
+    // var itemsToCreate = {
+    //   name: "li", 
+    //   food: "li", 
+    //   imgContainer: "li",
+    //   img: "img"
+    // }
+    var newListItem = document.createElement('ul');
+    newListItem.classList.add('cat')
+    var catName = document.createElement('li');
+    var catFood = document.createElement('li');
+    var imgContainer = document.createElement('li');
+    var img = document.createElement('img');
 
-  var newListItem = document.createElement('ul');
-  newListItem.classList.add('cat')
+    img.src = "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg"
+    img.width = 500;
+    imgContainer.appendChild(img);
 
-  var name = document.createElement('li');
-  var food = document.createElement('li');
-  var imgContainer = document.createElement('li');
-  var img = document.createElement('img');
-
-  img.src = "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg"
-  img.width = 500;
-  imgContainer.appendChild(img);
-
-  var section = document.getElementById('cats');
+    var section = document.getElementById('cats');
 
 
-  name.innerText = "Mr BigglesWorth";
-  food.innerText = "Kibble N'Bits"
+    catName.innerText = "Mr BigglesWorth";
+    catFood.innerText = "Kibble N'Bits"
 
 
-  newListItem.appendChild(name);
-  newListItem.appendChild(food);
-  newListItem.appendChild(imgContainer);
-  section.appendChild(newListItem);
+    newListItem.appendChild(catName);
+    newListItem.appendChild(catFood);
+    newListItem.appendChild(imgContainer);
+    section.appendChild(newListItem);
+  }
 
+  createNewCat();
 
 }
 
