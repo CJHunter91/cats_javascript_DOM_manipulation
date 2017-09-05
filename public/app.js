2e2b2cccd2bb1587e89de588815837ee442a481a
Chris Hunter <orkem2@gmail.com>
2017-09-05 08:59:28 +0100

    added some styling

--- public/app.js
+++ public/app.js
@@ -1,55 +1,40 @@
-
-var catsArray = [
-  {
-    name: "Mr BigglesWorth",
-    food: "Kibble N'Bits", 
-    img: "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg"
-  },
-  {
-    name: "KitKat",
-    food: "Tuna", 
-    img: "https://prickthyneighbour.files.wordpress.com/2013/05/cat-rob.png"
+var generateCats = function(){
+  for(cat of catsArray){
+    createNewCat(cat.name, cat.food, cat.img);
   }
-]
+}
 
+var generateCats = function(){
+  for(cat of catsArray){
+    createNewCat(cat.name, cat.food, cat.img);
+  }
+}
+
+var createNewCat = function(name, food, image){
+  var section = document.getElementById('cats');
+
+  var newListItem = document.createElement('ul');
+  newListItem.classList.add('cat')
+
+  var catName = document.createElement('li');
+  var catFood = document.createElement('li');
+  var imgContainer = document.createElement('li');
+  var img = document.createElement('img');
+
+  img.src = image
+  imgContainer.appendChild(img);
+
+  catName.innerText = name;
+  catFood.innerText = food;
+
+  newListItem.appendChild(imgContainer);
+  newListItem.appendChild(catName);
+  newListItem.appendChild(catFood);
+  section.appendChild(newListItem);
+}
 
 var app = function (){
-  
-  var createNewCat = function(name, food, image){
-    var section = document.getElementById('cats');
-
-    var newListItem = document.createElement('ul');
-    newListItem.classList.add('cat')
-
-    var catName = document.createElement('li');
-    var catFood = document.createElement('li');
-    var imgContainer = document.createElement('li');
-    var img = document.createElement('img');
-
-    img.src = image
-    img.width = 500;
-    imgContainer.appendChild(img);
-
-    catName.innerText = name;
-    catFood.innerText = food;
-
-    newListItem.appendChild(catName);
-    newListItem.appendChild(catFood);
-    newListItem.appendChild(imgContainer);
-    section.appendChild(newListItem);
-  }
-
-  var generateCats = function(){
-    for(cat of catsArray){
-      console.log(cat)
-      createNewCat(cat.name, cat.food, cat.img);
-    }
-  }
-
   generateCats();
-
-  // createNewCat("Mr BigglesWorth", "Kibble N'Bits", "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg");
-
 }
 
 window.onload = app;
---
+++ public/cat.js
@@ -0,0 +1,29 @@
+var catsArray = [
+  {
+    name: "Mr BigglesWorth",
+    food: "Kibble N'Bits", 
+    img: "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg"
+  },
+  {
+    name: "KitKat",
+    food: "Tuna", 
+    img: "https://prickthyneighbour.files.wordpress.com/2013/05/cat-rob.png"
+  },
+  {
+    name: "Boba", 
+    food: "Sock fluff", 
+    img: "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"
+  },
+  {
+    name: "Barnaby", 
+    food: "Tuna", 
+    img: "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"
+  },
+  {
+    name: "Max", 
+    food: "Whiskas Temptations", 
+    img: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"
+  }
+]
+
+// module.exports = catsArray;
